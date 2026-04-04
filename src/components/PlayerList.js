0803ff1bdd9fa10779df07f1a882b297ecfd9c42
Opeyemi OYEBOADE <oyeboadeopeyemi4e04e27f-la@gomycode.tn>
2026-04-04 22:18:08 +0100

    Fix import path for players in PlayerList.js

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,4 +1,4 @@
-import players from '../player'
+import players from '../Player'
 import Player from './Player'
 
 const newList = players.map(player => <Player key={player.id} name={player.name} image={player.image} team={player.team} age={player.age} nationality={player.nationality} jerseyNumber={player.jerseyNumber}/>)
